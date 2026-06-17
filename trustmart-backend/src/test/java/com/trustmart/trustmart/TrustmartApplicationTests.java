7e941f9fa9b8c9d5ebbc0acf095dc22e83753f97
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-06-17 11:49:02 +0545

    Initialize the backend project

---
+++ trustmart-backend/src/test/java/com/trustmart/trustmart/TrustmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustmart.trustmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
