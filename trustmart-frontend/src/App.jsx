9f98007c4c92fdf3432a857707e46d3e673bab61
acharyanisha1234 <acharyanisha2063gmail.com>
2026-06-18 16:38:28 +0545

    add basic forgot password page structure

---
+++ trustmart-frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import AdminRoutes from "./routes/AdminRoutes";
+import LoginPage from "./pages/LoginPage";
+import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+
+const App = () => {
+  const isAuthenticated = false;
+
+  return (
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/forgot-password" element={<ForgetPasswordPage />} />
+      
+      {/* Home Route - Only one "/" route */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      
+      {/* Admin Routes */}
+      <Route 
+        path="/admin/*" 
+        element={isAuthenticated ? <AdminRoutes /> : <Navigate to="/login" replace />} 
+      />
+      
+      {/* Catch all - Must be LAST */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+};
+
+export default App;
