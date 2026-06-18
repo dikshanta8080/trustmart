87a00d23399baacf6cc57235aeda286b53a09fc7
dilashathapa <dilashathapa15@gmail.com>
2026-06-18 16:44:55 +0545

    Add Sidebar component with user profile and navigation

--- trustmart-frontend/src/App.jsx
+++ trustmart-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         element={
           <>
             <Header />
-            
+            <Sidebar/>
           </>
         }
       />
