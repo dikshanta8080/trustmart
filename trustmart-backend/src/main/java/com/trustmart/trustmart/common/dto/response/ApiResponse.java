4d279e1eb352061bbba0bd4abc41c96ea8bbe0f7
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-06-18 15:43:17 +0545

    Implement Global Exception Handler

--- trustmart-backend/src/main/java/com/trustmart/trustmart/common/dto/response/ApiResponse.java
+++ trustmart-backend/src/main/java/com/trustmart/trustmart/common/dto/response/ApiResponse.java
@@ -9,14 +9,14 @@ public record ApiResponse<T>(
         T data
 ) {
     public static <T> ApiResponse<T> success(T data, String message) {
-        return new ApiResponse<>(
+        return new ApiResponse<T>(
                 true,
                 message,
                 data);
     }
 
     public static <T> ApiResponse<T> error(String message) {
-        return new ApiResponse<>(
+        return new ApiResponse<T>(
                 false,
                 message,
                 null);
